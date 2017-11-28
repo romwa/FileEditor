d9c7aa253f5e890e0ad907ca09ac38e8cd53cfbc
rwaisbort992 <rwaisbort992@hhs.win.fuhsd.org>
2017-11-28 09:51:31 -0800

    init

---
+++ src/romwa/runners/FileEditor.java
@@ -0,0 +1,11 @@
+package romwa.runners;
+
+public class FileEditor {
+
+	public static void main(String[] args) {
+		FileEditorRunner runner = new FileEditorRunner();
+		runner.openWindow();
+		
+	}
+
+}
